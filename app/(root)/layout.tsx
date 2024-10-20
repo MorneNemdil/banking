0e4d72ad92ec3d652d189e22c847c65e1f609ef6
MorneNemdil <mornenemdilbusiness@gmail.com>
2024-10-20 14:34:17 +0100

    added sidebar and sidebar for mobile

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,26 @@
+import MobileNavbar from "@/components/ui/mobile-navbar";
+import Sidebar from "@/components/ui/sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Morne', lastName: 'Nemdil' }
   return (
-    <main>
-      SIDEBAR
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image src={"/icons/logo.svg"} width={30} height={30} alt="logo" />
+          <div>
+            <MobileNavbar user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
---
+++ components/ui/sidebar.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import { sidebarLinks } from '@/constants'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
+
+const Sidebar = (props: SiderbarProps) => {
+    const pathName = usePathname();
+
+    return (
+        <section className='sidebar'>
+            <nav className='flex flex-col gap-4'>
+                <Link
+                    href='/'
+                    className='flex mb-12 curser-pointer items-center gap-2'
+                >
+                    <Image className='size-[24px] max-xl:size-14' src="/icons/logo.svg" width={34} height={34} alt='logo' />
+                    <h1 className='sidebar-logo'>Horizon</h1>
+                </Link>
+                {sidebarLinks.map(x => {
+                    const isCurrentPage = pathName === x.route || pathName.startsWith(x.route + "/")
+
+                    return (
+                        <Link
+                            href={x.route}
+                            key={x.label}
+                            className={cn('sidebar-link', { 'bg-bank-gradient': isCurrentPage })}
+                        >
+                            <div className='relative size-6'>
+                                <Image
+                                    src={x.imgURL}
+                                    alt={x.label}
+                                    fill
+                                    className={cn({ 'brightness-[3] invert-0': isCurrentPage })}
+                                />
+                            </div>
+                            <p className={cn('sidebar-label', { '!text-white': isCurrentPage })}>
+                                {x.label}
+                            </p>
+                        </Link>
+                    )
+                })}
+                USER
+            </nav>
+            FOOTER
+        </section>
+    )
+}
+
+export default Sidebar
